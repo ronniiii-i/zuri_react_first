ed4557ec807c9eeb37fd42d5c27759dd541c1fcb
Roni <ronniiii@yahoo.com>
2022-10-31 22:17:02 +0100

    restructure code

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,46 +1,27 @@
 import React from 'react'
-
+import data from '../data/links'
 import Link from "./Link";
 
 function Links() {
+    const link = data.map(item => {
+      return (
+        <Link
+          id={item.id}
+          name={item.name}
+          link={item.link}
+          title={
+            <>
+            <h6 style={{fontSize: '14px', textAlign: 'center'}}>{item.title}</h6>
+            
+            {item.subtext}
+          </>
+          }
+        />
+      )
+    })
   return (
     <section className='links flex column align-center full-width'>
-      <Link
-        id=''
-        name='My Twitter Link'
-        link='https://twitter.com/ronniiii_i'
-        title='title'
-      />
-      <Link
-        id='btn__zuri'
-        name='Zuri Team'
-        link='https://training.zuri.team/'
-        title='title'
-      />
-      <Link
-        id='books'
-        name='Zuri Books'
-        link='http://books.zuri.team/'
-        title='title'
-      />
-      <Link
-        id='book__python'
-        name='Python Books'
-        link='https://books.zuri.team/python-for-beginners?ref_id=Roni'
-        title='title'
-      />
-      <Link
-        id='pitch'
-        name='Background Check for Coders'
-        link='https://background.zuri.team/'
-        title='title'
-      />
-      <Link
-        id='book__design'
-        name='Design Books'
-        link='https://books.zuri.team/design-rules'
-        title='title'
-      />
+      {link}
     </section>
   )
 }
---
+++ src/data/links.js
@@ -0,0 +1,51 @@
+
+const links = [
+    {
+      name: "Twitter Link",
+      link: "https://twitter.com/ronniiii_i",
+      id: "twitter__link",
+      title: "",
+      subtext: "This is a link to take you to my twitter handle ",
+    },
+    {
+      name: "Zuri Team",
+      link: "https://training.zuri.team/",
+      id: "btn__zuri",
+      title: "",
+      subtext: "Meet the Zuri Family",
+    },
+    {
+      name: "Zuri Books",
+      link: "http://books.zuri.team",
+      id: "books",
+      title: "Zuris Library of Books",
+      subtext:
+        "Get access to the collection of limited edition tech and career related books offered by zuri to help your journey as a developer ",
+    },
+    {
+      name: "Python Books",
+      link: "https://books.zuri.team/",
+      id: "book__python",
+      title: "",
+      subtext: "Grab a copy of the python for beginners book",
+    },
+    {
+      name: "Background Check for coders",
+      link: "https://background.zuri.team",
+      id: "pitch",
+      title: "",
+      subtext:
+        "Tech talent hunt is a program where you get to test your skills in your area of expertise, learn from mentors, and watch others do amazing things with tech",
+    },
+    {
+      name: "Design Books",
+      link: "https://books.zuri.team/design-rules",
+      id: "book__design",
+      title: "",
+      subtext:
+        "get UI/UX books for writing copy that's engaging and conversational—and aligned with the goals of both the user andthe business",
+    },
+  ];
+  
+  
+  export default links 
